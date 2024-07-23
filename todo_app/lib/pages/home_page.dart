4b3559ac3ba86d39b315d5518994a722ae9eaca9
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-07-23 22:44:50 +0800

    Added todo tile

--- todo_app/lib/pages/home_page.dart
+++ todo_app/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -10,11 +11,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Exercise", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxOnChanged(bool? value, int idx) {
+    setState(() {
+      toDoList[idx][1] = !toDoList[idx][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[200],
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green[300],
+        title: const Text('TO DO'),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1], 
+            onChanged: (value) => checkBoxOnChanged(value, index), 
+            );
+        })
     );
   }
 }
